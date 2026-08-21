2ff42d414a1245744271c03f117e52e48de91d86
VialsShiny <vialsvialatou@gmail.com>
2026-08-21 19:15:49 +0200

    calc fahr to celsus

---
+++ calc/while/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main() {
+  float fahr;
+  float celsus;
+  int min, max, interval;
+  min = 0;
+  max = 300;
+  interval = 20;
+  fahr = min;
+
+  while (fahr <= max) {
+    celsus = (fahr - 32) * 5/9;
+    printf("Fahr : %.0f\tCelsus : %.2f\n", fahr, celsus);
+    fahr += interval;
+  }
+  system("PAUSE");
+}
