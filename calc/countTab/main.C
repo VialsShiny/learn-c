327e05c6ce2c8600783b286070203d420003ce64
VialsShiny <vialsvialatou@gmail.com>
2026-08-23 16:10:21 +0200

    added print_array function

--- calc/countTab/main.C
+++ calc/countTab/main.C
@@ -2,14 +2,27 @@
 #include <stdlib.h>
 #include <stddef.h>
 
+void print_array(int* tab, int length) {
+  printf("{");
+  for (size_t i = 0; i < length; i++) {
+    if (i + 1 != length) {
+      printf("'%d', ", tab[i]);
+    } else {
+      printf("'%d'", tab[i]);
+    }
+  };
+  printf("}\n");
+}
+
 int main() {
-  int tab[100] = {15, 36, 59, 45}; // 155
+  int tab[] = {15, 36, 59, 45}; // 155
   size_t length = sizeof(tab) / sizeof(tab[0]);
   int total = 0;
   for (size_t i = 0; i < length; i++) {
     total += tab[i];
   }
   
-  printf("%d\n", total);
+  print_array(tab, length);
+  printf("Result : %d\n", total);
   system("PAUSE");
 }
