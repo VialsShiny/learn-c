21e1e42ece516dee8faad0bf3e37b14532312dee
VialsShiny <vialsvialatou@gmail.com>
2026-08-23 15:24:28 +0200

    Clean Code

--- calc/countTab/main.C
+++ calc/countTab/main.C
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
 
 int main() {
   int tab[100] = {15, 36, 59, 45}; // 155
+  size_t length = sizeof(tab) / sizeof(tab[0]);
   int total = 0;
-  for (int i = 0; i < sizeof(tab) / sizeof(tab[0]); i++) {
+  for (size_t i = 0; i < length; i++) {
     total += tab[i];
   }
+  
   printf("%d\n", total);
   system("PAUSE");
 }
