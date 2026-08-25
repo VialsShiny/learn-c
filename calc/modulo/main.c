23bf90a8362c7793f26fe2526cff0c797f1d6994
VialsShiny <vialsvialatou@gmail.com>
2026-08-26 01:13:20 +0200

    added anti buffer

--- calc/modulo/main.c
+++ calc/modulo/main.c
@@ -10,23 +10,32 @@ int main() {
   printf("--- PAIR OU IMPAIR ---\n\n");
   while (choice == 'o') {
     printf("Veuillez entree un nombre compris entre 1 & 99 : ");
-    result = scanf("%d", &number);  
-    printf("\n");
+    result = scanf(" %d", &number);  
     if (result) {
+      printf("\n");
       if(number % 2 != 0) {
         printf("Impair : %d\n", number);
       } else {
         printf("Pair : %d\n", number);
       }
     } else {
+      int c;
+      while ((c = getchar()) != '\n' && c != EOF) {
+        c = getchar();
+      }
       printf("Erreur : ce n'est pas un nombre !\n");
     }
+    result = 0;
     while(!result) {
       printf("Voulez-vous continuer ? (o/n) ");
-      result = scanf("%c", &choice);
-      printf("\n");
+      result = scanf(" %c", &choice);
       if (result && choice != 'o' && choice != 'n') {
+        printf("\n");
         result = 0;
+        int c;
+        while ((c = getchar()) != '\n' && c != EOF) {
+          c = getchar();
+        }
         printf("Erreur : Veuillez choisir entre o/n !\n");
       } else {
         result = 1;
