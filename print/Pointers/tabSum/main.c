21cc6f040a258500696ffe21743e9c2fbb5fa9f7
VialsShiny <vialsvialatou@gmail.com>
2026-08-31 00:54:33 +0200

    Added tabSum

---
+++ print/Pointers/tabSum/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+int main() {
+  int tab[] = {10, 20, 30, 40, 50};
+  int sum = 0;
+  size_t length = sizeof(tab) / sizeof(tab[0]);
+
+  int *p = tab;
+  int *end = tab + length;
+
+  while(p < end) {
+    sum += *p;
+    p++;
+  }
+
+  printf("%d\n", sum);
+  system("PAUSE");
+}
