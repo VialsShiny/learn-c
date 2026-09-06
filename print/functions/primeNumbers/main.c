064caa4f0d63db71bafb873547ddd14cff325c8a
VialsShiny <vialsvialatou@gmail.com>
2026-09-06 14:37:22 +0200

    added functions primeNumber checker

--- print/functions/primeNumbers/main.c
+++ print/functions/primeNumbers/main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
+#include <math.h>
+
+int checkPrimeNumber(int nbr) {
+  double squareRoot = sqrt(nbr);
+  int flag = 0;
+
+  if (nbr <= 1)
+    flag = 0;
+
+  for (size_t i = 2; i < squareRoot; i++) {
+    if (nbr % i == 0) {
+      flag = 0;
+      break;
+    }
+    
+    flag = 1;
+  }
+
+  return flag;
+}
 
 int main() {
   printf("Here i'm coding !");
